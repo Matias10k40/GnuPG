cd3c197e50b0ae2a331458db8667898671b4e286
Matias10k40 <maquinterosye@uide.edu.ec>
2026-03-27 03:15:13 +0000

    Generate GPG key pair

--- script_gnupg.sh
+++ script_gnupg.sh
@@ -1,8 +1,7 @@
-# A2 - Check GPG version
-gpg --version#!/bin/bash
-# =============================================================================
-# BLOCK A — Environment Setup
-# =============================================================================
-
-# A1 - Verify GPG is installed
+A3 - Generate GPG key pair
+gpg --full-generate-key
+A2 - Check GPG version
+gpg --version/bin/bash
+BLOCK A — Environment Setup
+A1 - Verify GPG is installed
 which gpg
